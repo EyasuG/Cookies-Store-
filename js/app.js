a28a219ac48533534f0e8b6c4e76d359c5066bcc
EyasuG <jgebrehiwot@gmail.com>
2018-10-30 01:19:52 -0400

    The Second Question solved"

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 let elBody = document.getElementById('myBody')
 let elBody1= document.getElementById('myBody1')
-let elBody2= document.getElementById("myBody2")
+let elBody2= document.getElementById("myBody3")
 
 
 
@@ -10,7 +10,6 @@ let elBody2= document.getElementById("myBody2")
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am','1pm', '2pm','3pm','4pm','5pm','6pm','7pm']
 let cookies = []
 
-// this the location of cookies store in Silver-Spring
 // this the location of cookies store in Silver-Spring
 
 let Silverspring= {
@@ -29,7 +28,6 @@ let Silverspring= {
 let elSilverspring = document.createElement('h3')
 elBody.appendChild(elSilverspring)
 elSilverspring.innerText = Silverspring.location
-
 //display store information into html
 // let elList = document.createElement('ul')
 // elBody.appendChild(elList)
@@ -40,17 +38,17 @@ for (let i =0; i<hours.length; i++){
   elListItem.innerText = hours[i] + ': ' + Silverspring.cookiesPerHour() 
 }
 
-// this the location of cookies store in Hyattsville 
+// this the location of cookies store in Adelphi 
 
 
-let Hyattsville= {
-  location:'hyattsville',
+let Adelphi= {
+  location:'Adelphi',
   minNumCus: 35,
   maxNumCus: 150,
   cookiesPerCus:15,
   cookiesPerHour: function() {
     let randomNumCookies = Math.ceil(Math.random() * (this.maxNumCus-this.minNumCus) + this.minNumCus)
-    //console.log(this.cookiesPerHour.randomNumCookies)
+    console.log(this.cookiesPerHour.randomNumCookies)
     return randomNumCookies * this.cookiesPerCus
     alert(cookiesPerHour())
   }
@@ -58,54 +56,54 @@ let Hyattsville= {
   
   
   //display store-1 name in h2
-  let elHyattsville = document.createElement('h3')
-  elBody1.appendChild(elHyattsville)
-  elHyattsville.innerText = Hyattsville.location
+  let elAdelphi = document.createElement('h3')
+  elBody1.appendChild(elAdelphi)
+  elAdelphi.innerText = Adelphi.location
   
   //display store information into html
-  // let elList = document.createElement('ul')
-  // elBody.appendChild(elList)
+  let elList = document.createElement('ul')
+  elBody1.appendChild(elList)
   
   
   
   for (let i =0; i<hours.length; i++){
-    console.log(hours[i],'Total # of Cookies/hr in the location of', Hyattsville.location, Hyattsville.cookiesPerHour())
+    console.log(hours[i],'Total # of Cookies/hr in the location of', Adelphi.location, Adelphi.cookiesPerHour())
     let elListItem = document.createElement('ul')
-    elBody1.appendChild(elListItem)
-    elListItem.innerText = hours[i] + ': ' + Hyattsville.cookiesPerHour() 
+    elBody1.appendChild(elAdelphi)
+    elListItem.innerText = hours[i] + ': ' + Adelphi.cookiesPerHour() 
   }
 
-  // Store 3 in Greenbelt
+  // Store 3 in Bethesda 
 
-  let Greenbelt= {
-    location:'greenbelt',
-    minNumCus: 22,
-    maxNumCus: 120,
-    cookiesPerCus:12,
-    cookiesPerHour: function() {
-      let randomNumCookies = Math.ceil(Math.random() * (this.maxNumCus-this.minNumCus) + this.minNumCus)
-      //console.log(this.cookiesPerHour.randomNumCookies)
-      return randomNumCookies * this.cookiesPerCus
-      alert(cookiesPerHour())
-    }
-    }
+  // let Bethesda= {
+  //   location:'Bethesda',
+  //   minNumCus: 22,
+  //   maxNumCus: 120,
+  //   cookiesPerCus:12,
+  //   cookiesPerHour: function() {
+  //     let randomNumCookies = Math.ceil(Math.random() * (this.maxNumCus-this.minNumCus) + this.minNumCus)
+  //     //console.log(this.cookiesPerHour.randomNumCookies)
+  //     return randomNumCookies * this.cookiesPerCus
+  //     alert(cookiesPerHour())
+  //   }
+  //   }
     
     
-    //display store-1 name in h2
-    let elGreenbelt = document.createElement('h3')
-    elBody2.appendChild(elGreenbelt)
-    elGreenbelt.innerText = Greenbelt.location
+  //   //display store-1 name in h2
+  //   let elBethesda= document.createElement('h3')
+  //   elBody2.appendChild(elBethesda)
+  //   elBethesda.innerText = Bethesda.location
     
-    //display store information into html
-    // let elList = document.createElement('ul')
-    // elBody.appendChild(elList)
+  //   //display store information into html
+  //   //let elList = document.createElement('ul')
+  //   elBody.appendChild(elList)
     
     
     
-    for (let i =0; i<hours.length; i++){
-      console.log(hours[i],'Total # of Cookies/hr in the location of', Greenbelt.location, Greenbelt.cookiesPerHour())
-      let elListItem = document.createElement('ul')
-      elBody2.appendChild(elListItem)
-      elListItem.innerText = hours[i] + ': ' + Greenbelt.cookiesPerHour() 
-    }
+  //   for (let i =0; i<hours.length; i++){
+  //     console.log(hours[i],'Total # of Cookies/hr in the location of', Bethesda.location, Bethesda.cookiesPerHour())
+  //     let elListItem = document.createElement('ul')
+  //     elBody2.appendChild(elListItem)
+  //     elListItem.innerText = hours[i] + ': ' + Bethesda.cookiesPerHour() 
+  //   }
   
